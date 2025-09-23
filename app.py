684ce7eb6a160da429c5f7158a5e72d82adf4469
Luv Sharma <luvsharma105@gmail.com>
2025-09-24 00:41:22 +0530

    punkt commit

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+nltk.download("punkt")
+nltk.download("punkt_tab")
+
 ps = PorterStemmer()
 
 def transform_text(text):
